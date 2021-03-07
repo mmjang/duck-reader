2a1e3862baa1d089cdf95f0869af026fa50fa59d
mmjang <752515918@qq.com>
2021-03-07 22:57:32 +0800

    测试capacitor保存文件的可行性

--- src/components/homebanner/HomeBanner.tsx
+++ src/components/homebanner/HomeBanner.tsx
@@ -1,10 +1,47 @@
 import { useHistory } from "react-router-dom";
 import "./HomeBanner.css";
+import FileSaver, { saveAs } from "file-saver";
+
+import {
+  Plugins,
+  FilesystemDirectory,
+  FilesystemEncoding,
+} from "@capacitor/core";
+
+const { Filesystem, Share } = Plugins;
 
 export default function HomeBanner() {
   const history = useHistory();
-  function navigateToCardList() {
-    history.push("/cardlist");
+  async function navigateToCardList() {
+    // history.push("/cardlist");
+    // const blob = new Blob(["Hello, world!"], {
+    //   type: "text/plain;charset=utf-8",
+    // });
+    // var reader = new FileReader();
+    // reader.onloadend = async () => {
+    //   var base64data = reader.result;
+    //   console.log(base64data);
+    //   try {
+    //     const result = await Filesystem.writeFile({
+    //       path: "text.txt",
+    //       data: base64data as string,
+    //       directory: FilesystemDirectory.Documents,
+    //     });
+    //     const uri = await Filesystem.getUri({
+    //       path: "text.txt",
+    //       directory: FilesystemDirectory.Documents,
+    //     });
+    //     Share.share({
+    //       title: "测试",
+    //       url: uri.uri,
+    //       dialogTitle: "what the fuck",
+    //     });
+    //     console.log("Wrote file", result);
+    //   } catch (e) {
+    //     console.log("write failed", e);
+    //   }
+    // };
+    // reader.readAsDataURL(blob);
   }
   return (
     <div className="home-banner">

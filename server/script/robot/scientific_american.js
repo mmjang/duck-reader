50d06bd300d379ce5f126d5438f7bb091060f18c
mmjang <752515918@qq.com>
2021-10-02 18:03:06 +0800

    feature: 科学美国人

---
+++ server/script/robot/scientific_american.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+
+const username = "科学美国人";
+const password = "paitsaml";
+
+(async () => {
+  const token = (
+    await axios.post("https://duck-reader.com/api/login", {
+      username,
+      password,
+    })
+  ).data.data.token;
+  console.log(token);
+  const text = (await axios.get("rss.sciam.com/ScientificAmerican-Global"))
+    .data;
+  const pattern = /\"(https\/\/www\.scientificamerican\.com\/article\/.+?)\"/;
+  for (let line of text.split("\n").reverse()) {
+    if (line.includes("topic-title")) {
+      const match = line.match(pattern);
+      if (match) {
+        const url = match[1];
+        console.log(url);
+        try {
+          const article = await axios.post(
+            `https://duck-reader.com/api/submitArticle`,
+            {
+              url,
+              disableDuplicate: true,
+            },
+            {
+              headers: {
+                token,
+              },
+            }
+          );
+          console.log(article);
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    }
+  }
+})();

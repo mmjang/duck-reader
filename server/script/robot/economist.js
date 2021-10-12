1b39921b4fde4bcf79209c1d7f9bf2a2d0329d0f
mmjang <752515918@qq.com>
2021-10-13 00:38:15 +0800

    feature: economist robot

--- server/script/robot/economist.js
+++ server/script/robot/economist.js
@@ -1,21 +1,49 @@
 const axios = require("axios");
 const runRobot = require("./runRobot");
 
+const sectionList = [
+  "https://www.economist.com/the-world-this-week/rss.xml",
+  "https://www.economist.com/leaders/rss.xml",
+  "https://www.economist.com/briefing/rss.xml",
+  "https://www.economist.com/special-report/rss.xml",
+  "https://www.economist.com/britain/rss.xml",
+  "https://www.economist.com/europe/rss.xml",
+  "https://www.economist.com/united-states/rss.xml",
+  "https://www.economist.com/the-americas/rss.xml",
+  "https://www.economist.com/middle-east-and-africa/rss.xml",
+  "https://www.economist.com/asia/rss.xml",
+  "https://www.economist.com/china/rss.xml",
+  "https://www.economist.com/international/rss.xml",
+  "https://www.economist.com/business/rss.xml",
+  "https://www.economist.com/finance-and-economics/rss.xml",
+  "https://www.economist.com/science-and-technology/rss.xml",
+  "https://www.economist.com/books-and-arts/rss.xml",
+  "https://www.economist.com/obituary/rss.xml",
+  "https://www.economist.com/graphic-detail/rss.xml",
+  "https://www.economist.com/economic-and-financial-indicators/rss.xml",
+].reverse();
+
 runRobot({
   username: "静寂血刃",
   password: "paitsam777",
   urlExtractor: async () => {
-    const text = (
-      await axios.get(
-        "https://www.economist.com/science-and-technology/rss.xml"
-      )
-    ).data;
-    console.log(text);
-    const urls = [];
-    for (let line of text.split("\n")) {
-      let m = line.match(/<link>(https:\/\/www\.economist\.com.+?)<\/link>/);
-      if (m) {
-        urls.push(m[1]);
+    for (let section of sectionList) {
+      const text = (await axios.get(section)).data;
+      console.log(text);
+      const urls = [];
+      for (let line of text.split("\n").reverse()) {
+        let m = line.match(/<link>(https:\/\/www\.economist\.com.+?)<\/link>/);
+        let dateMatch = line.match(/([0-9]{4})\/([0-9]{2})\/([0-9]{2})/);
+        if (m && dateMatch) {
+          let [year, month, day] = [
+            dateMatch[1],
+            dateMatch[2],
+            dateMatch[3],
+          ].map((s) => parseInt(s));
+          if (year > 2021 || (year == 2021 && month >= 10)) {
+            urls.push(m[1]);
+          }
+        }
       }
     }
     return urls;
